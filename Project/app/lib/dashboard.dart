208acc757ae792ce2c1b983a16caf5ff13ff9d49
navendunandu <navendunandu@gmail.com>
2024-01-29 22:50:47 +0530

    Registration Login

--- Project/app/lib/dashboard.dart
+++ Project/app/lib/dashboard.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:edupulse/my_profile.dart';
 import 'package:edupulse/view_candidate.dart';
 import 'package:edupulse/view_result.dart';
 import 'package:edupulse/vote.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
 
@@ -11,11 +14,39 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  String name = 'Loading...';
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  Future<void> getData() async {
+    final user = FirebaseAuth.instance.currentUser;
+    final userId = user?.uid;
+    QuerySnapshot<Map<String, dynamic>> querySnapshot = await FirebaseFirestore
+        .instance
+        .collection('tbl_studentregister')
+        .where('Student_id', isEqualTo: userId)
+        .limit(1)
+        .get();
+    if (querySnapshot.docs.isNotEmpty) {
+      setState(() {
+        name = querySnapshot.docs.first['Student_name'];
+      });
+    } else {
+      setState(() {
+        name = 'Error Loading Data';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('EDUPULSE'),
+        title: const Text('EDUPULSE'),
         actions: [
           IconButton(
             onPressed: () {
@@ -24,11 +55,11 @@ class _DashboardState extends State<Dashboard> {
                 MaterialPageRoute(builder: (context) => const MyProfile()),
               );
             },
-            icon: Icon(Icons.person), // Replace with the desired icon
+            icon: const Icon(Icons.person), // Replace with the desired icon
           ),
         ],
       ),
-       body: Padding(
+      body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Container(
           padding: const EdgeInsets.all(20.0),
@@ -43,48 +74,102 @@ class _DashboardState extends State<Dashboard> {
               const SizedBox(
                 height: 20,
               ),
-              Center(child: Text('HELLO,username',style: TextStyle(fontSize: 25.0),)),
+              Center(
+                  child: Column(
+                children: [
+                  Text(
+                    'Hello,',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    name,
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+                  )
+                ],
+              )),
               const SizedBox(
-                height: 50,
+                height: 20,
               ),
               GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Vote(),)); 
-              },
-              child:Container(
-                decoration:BoxDecoration (borderRadius: BorderRadius.circular(15),color:Colors.lightBlueAccent,),
-                height: 100,
-                width: 100,
-                child: Center(child: Text('VOTE',style: TextStyle(fontSize: 30,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),)),
-              ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Vote(),
+                      ));
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.lightBlueAccent,
+                  ),
+                  height: 100,
+                  width: 100,
+                  child: const Center(
+                      child: Text(
+                    'VOTE',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold),
+                  )),
+                ),
               ),
               const SizedBox(
                 height: 20,
               ),
               GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const viewcandidate(),)); 
-              },
-              child:Container(
-                decoration:BoxDecoration (borderRadius: BorderRadius.circular(15),color:Colors.lightBlueAccent,),
-                height: 100,
-                width: 100,
-                child: Center(child: Text('VIEW CANDIDATE',style: TextStyle(fontSize: 30,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),)),
-              ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const viewcandidate(),
+                      ));
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.lightBlueAccent,
+                  ),
+                  height: 100,
+                  width: 100,
+                  child: const Center(
+                      child: Text(
+                    'VIEW CANDIDATE',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold),
+                  )),
+                ),
               ),
               const SizedBox(
                 height: 20,
               ),
               GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const ViewResult(),)); 
-              },
-              child:Container(
-                decoration:BoxDecoration (borderRadius: BorderRadius.circular(15),color:Colors.lightBlueAccent,),
-                height: 100,
-                width: 100,
-                child: Center(child: Text('VIEW RESULT',style: TextStyle(fontSize: 30,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),)),
-              ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ViewResult(),
+                      ));
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.lightBlueAccent,
+                  ),
+                  height: 100,
+                  width: 100,
+                  child: const Center(
+                      child: Text(
+                    'VIEW RESULT',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold),
+                  )),
+                ),
               ),
             ],
           ),
@@ -92,4 +177,4 @@ class _DashboardState extends State<Dashboard> {
       ),
     );
   }
-} 
+}
--- Project/app/lib/main.dart
+++ Project/app/lib/main.dart
@@ -1,25 +1,19 @@
 import 'package:edupulse/firebase_options.dart';
 import 'package:edupulse/splash_screen.dart';
 import 'package:flutter/material.dart';
-//file
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
-
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home:splashscreen()
-    );
+    return const MaterialApp(home: splashscreen());
   }
 }
